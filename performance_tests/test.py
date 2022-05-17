42669bc1193e48b55f783c2de68a55fedf788bb0
lyriccoder <e00533045@china.huawei.com>
2022-05-17 15:27:02 +0300

    W

--- performance_tests/test.py
+++ performance_tests/test.py
@@ -102,7 +102,7 @@ if __name__ == '__main__':
     # df = pd.DataFrame(columns=['file', 'secs', 'commit_id', 'commit_time'])
     # run_cmd_and_print_output('git clone ')
     json_d = run_perf_test(None, None, args.dataset_path)
-    with open(args.commit_id + '.csv', encoding='utf-8') as w:
+    with open(args.commit_id + '.csv', 'w', encoding='utf-8') as w:
         json.dump(json_d)
     # for mr_i in merge_requests_lst:
     #     commit_id, time_str = mr_i.split(' ', maxsplit=1)
