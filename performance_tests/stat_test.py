96f50671a1354b2fae3d491ee6e110c17438b063
lyriccoder <e00533045@china.huawei.com>
2022-05-18 15:47:47 +0300

    Fix

--- performance_tests/stat_test.py
+++ performance_tests/stat_test.py
@@ -1,14 +1,5 @@
-from functools import reduce
-from typing import Set
 from unittest import TestCase, main
 
-from program_graphs.adg.parser.java.parser import parse  # type: ignore
-
-from slicing.block.block import gen_block_slices
-from slicing.block.block import get_entry_candidates, get_node_lines, mk_declared_variables_table
-from slicing.block.filters import at_least_one_block_stmt, last_or_next_statement_is_control
-from slicing.block.state import State
-from slicing.block.utils import get_occupied_line_range, count_ncss, find_blank_and_full_comment_lines
 import numpy as np
 from scipy.stats import ttest_ind
 from pathlib import Path
