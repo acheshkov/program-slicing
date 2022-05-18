f75058ecfccf757362db47440c63998b1fb81e4c
lyriccoder <e00533045@china.huawei.com>
2022-05-18 14:34:57 +0300

    Fix versionof test

--- performance_tests/stat_test.py
+++ performance_tests/stat_test.py
@@ -11,20 +11,39 @@ from slicing.block.state import State
 from slicing.block.utils import get_occupied_line_range, count_ncss, find_blank_and_full_comment_lines
 import numpy as np
 from scipy.stats import ttest_ind
+from pathlib import Path
+import argparse
 
 
-class StatTtest(TestCase):
+def is_avg_larger(cur_csv: Path, prev_csv: Path) -> None:
+    prev_df = pd.read_csv(cur_csv)
+    cur_df = pd.read_csv(prev_csv)
+    prev_mean = prev_df['diff'].mean()
+    cur_mean = cur_df['diff'].mean()
+    prev_mean_scaled = np.interp(prev_mean, (prev_mean.min(), prev_mean.max()), (-1, +1))
+    cur_mean_scaled = np.interp(cur_mean, (cur_mean.min(), cur_mean.max()), (-1, +1))
+    print('not scaled prev ', prev_df['diff'])
+    print('scaled prev', prev_mean_scaled)
+    print('not scaled cur ', cur_df['diff'])
+    print('scaled cur', cur_mean_scaled)
+    t_statistic, pvalue = ttest_ind(prev_mean_scaled, cur_mean_scaled)
+    print(f'Pval {pvalue}')
+    # self.assertLess(pvalue, 0.01)
 
-    def is_avg_larger(self) -> None:
-        prev_df = pd.read_csv('prev.csv')
-        cur_df = pd.read_csv('cur.csv')
-        prev_mean = prev_df['diff'].mean()
-        cur_mean = cur_df['diff'].mean()
-        prev_mean_scaled = np.interp(prev_mean, (prev_mean.min(), prev_mean.max()), (-1, +1))
-        cur_mean_scaled = np.interp(cur_mean, (cur_mean.min(), cur_mean.max()), (-1, +1))
-        print('not scaled prev ', prev_df['diff'])
-        print('scaled prev', prev_mean_scaled)
-        print('not scaled cur ', cur_df['diff'])
-        print('scaled cur', cur_mean_scaled)
-        t_statistic, pvalue = ttest_ind(prev_mean_scaled, cur_mean_scaled)
-        self.assertLess(pvalue, 0.01)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Run performance testing for program-slicing repo with certain commit id'
+    )
+    parser.add_argument(
+        '--prev_csv',
+        type=Path,
+        required=True
+    )
+    parser.add_argument(
+        '--cur_csv',
+        type=Path,
+        required=True
+    )
+    args = parser.parse_args()
+    is_avg_larger(args.cur_csv, args.prev_csv)
